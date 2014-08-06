75d76ecdeda93bd35e1b87577628b18b10feb6f1
LeeW <cl87654321@gmail.com>
2014-08-06 21:04:26 +0800

    finish turn functions into class

--- pttcrawler.py
+++ pttcrawler.py
@@ -1,5 +1,4 @@
 import re
-import sys
 import json
 import requests
 from time import sleep
@@ -8,19 +7,20 @@ from bs4 import BeautifulSoup
 
 class pttCrawler:
     def __init__(self):
-        self.pttURL = "http://www.ptt.cc/bbs/"
-        pass
+        self.pttURL = "http://www.ptt.cc/"
+        self.result = list()
 
-    def crawl(self, start, end, boardName="Gossiping"):
-        page = start
-        times = end-start+1
-        articleID = 0
+    def crawl(self, start=None, end=None, boardName="Gossiping"):
+        start = self.getLastPageNum(boardName) if start is None else start
+        end = self.getLastPageNum(boardName) if end is None else end
+        articleID = int()
 
-        for t in range(times):
+        for page in range(start, end+1):
             print('index is ' + str(page))
-            req = requests.get(url=self.pttURL + boardName+"/index" + str(page) + ".html",
+            req = requests.get(url=self.pttURL+"bbs/"+boardName+"/index"+str(page)+".html",
                                cookies={"over18": "1"})
             soup = BeautifulSoup(req.text)
+
             for tag in soup.find_all("div", "r-ent"):
                 try:
                     link = str(tag.find_all("a"))
@@ -31,12 +31,12 @@ class pttCrawler:
                 except:
                     pass
             sleep(0.2)
-            page += 1
+        return self.result
 
     def parseContent(self, link, articleID):
         req = requests.get(url=str(link), cookies={"over18": "1"})
         soup = BeautifulSoup(req.text)
-        print(req)
+        print(req.url)
 
         # author
         author = soup.find(id="main-container").contents[1].contents[0].contents[1].string.replace(' ', '')
@@ -56,34 +56,36 @@ class pttCrawler:
         a = a[4].split("<span class=\"f2\">※ 發信站: 批踢踢實業坊(ptt.cc),")
         content = a[0].replace(' ', '').replace('\n', '').replace('\t', '')
         # message
-        num, g, b, n, message = 0, 0, 0, 0, {}
+        messageNum, g, b, n, message = int(), int(), int(), int(), dict()
         for tag in soup.find_all("div", "push"):
-            num += 1
+            messageNum += 1
             push_tag = tag.find("span", "push-tag").string.replace(' ', '')
             push_userid = tag.find("span", "push-userid").string.replace(' ', '')
-            push_content = tag.find("span", "push-content").string.replace(' ', '').replace('\n', '').replace('\t', '')
+            try:
+                push_content = tag.find("span", "push-content").string.replace(' ', '').replace('\n', '').replace('\t', '')
+            except:
+                # if there is no content
+                push_content = ""
             push_ipdatetime = tag.find("span", "push-ipdatetime").string.replace('\n', '')
 
-            message[num] = {"狀態": push_tag, "留言者": push_userid, "留言內容": push_content, "留言時間": push_ipdatetime}
+            message[messageNum] = {"狀態": push_tag, "留言者": push_userid, "留言內容": push_content, "留言時間": push_ipdatetime}
             if push_tag == '推 ':
                 g += 1
             elif push_tag == '噓 ':
                 b += 1
             else:
                 n += 1
-            messageNum = {"g": g, "b": b, "n": n, "all": num}
-            # json-data
-            d = {"a_ID": articleID,
-                 "b_作者": author,
-                 "c_標題": title,
-                 "d_日期": date,
-                 "e_ip": ip,
-                 "f_內文": content,
-                 "g_推文": message,
-                 "h_推文總數": messageNum}
-            json_data = json.dumps(d, ensure_ascii=False, indent=4, sort_keys=True)+','
+            messageN = {"g": g, "b": b, "n": n, "all": messageNum}
 
-            self.store(json_data)
+        data = {"a_ID": articleID,
+                "b_作者": author,
+                "c_標題": title,
+                "d_日期": date,
+                "e_ip": ip,
+                "f_內文": content,
+                "g_推文": message,
+                "h_推文總數": messageN}
+        self.result.append(data)
 
     def getLastPageNum(self, boardName="Gossiping"):
         resp = requests.get(url="http://www.ptt.cc/bbs/"+boardName+"/index.html",
@@ -93,15 +95,12 @@ class pttCrawler:
             prevPage = int(re.search("[0-9]+", prevPageIdentifier).group())
         return prevPage+1
 
-    def store(self, data):
-        with open('data.json', 'a') as f:
-            f.write(data)
+    def export(self, fileName="output.json"):
+        with open(fileName, 'w') as f:
+            json.dump(self.result, f, ensure_ascii=False, indent=4, sort_keys=True)
 
-    def export(self):
-        self.store('[')
-        self.crawl(int(sys.argv[1]), int(sys.argv[2]))
-        self.store(']')
-        with open('data.json', 'r') as f:
-            p = f.read()
-        with open('data.json', 'w') as f:
-            f.write(p.replace(',]', ']'))
+
+if __name__ == '__main__':
+    ptt = pttCrawler()
+    ptt.crawl()
+    ptt.export()
