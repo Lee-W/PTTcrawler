f588cab119af38cfa10bdf0dbdec6bb04ac2625c
LeeW <cl87654321@gmail.com>
2014-08-06 23:42:52 +0800

    refine pttcrawler

--- pttcrawler.py
+++ pttcrawler.py
@@ -10,35 +10,36 @@ class PTTCrawler:
         self.pttURL = "http://www.ptt.cc/"
         self.result = list()
 
-    def crawl(self, start=None, end=None, boardName="Gossiping", displayMessage=True):
-        start = self.getLastPageNum(boardName) if start is None else start
-        end = self.getLastPageNum(boardName) if end is None else end
+    def crawl(self, start=None, end=None, boardName="Gossiping", displayProgress=True):
+        start = self.__countLastPageNum(boardName) if start is None else start
+        end = self.__countLastPageNum(boardName) if end is None else end
+
         articleID = int()
 
         for page in range(start, end+1):
-            if displayMessage is True:
+            if displayProgress is True:
                 print('index is ' + str(page))
-            req = requests.get(url=self.pttURL+"bbs/"+boardName+"/index"+str(page)+".html",
-                               cookies={"over18": "1"})
+
+            boardURL = self.pttURL+"bbs/"+boardName+"/index"+str(page)+".html"
+            req = requests.get(url=boardURL, cookies={"over18": "1"})
             soup = BeautifulSoup(req.text)
 
             for tag in soup.find_all("div", "r-ent"):
                 try:
-                    link = str(tag.find_all("a"))
-                    link = link.split("\"")
+                    link = str(tag.find_all("a")).split("\"")
                     link = self.pttURL + link[1]
-                    articleID = articleID+1
-                    self.parseContent(link, articleID, displayMessage)
+                    articleID = articleID + 1
+                    self.__parseArticle(link, articleID, displayProgress)
                 except:
                     print ("error")
                     pass
             sleep(0.2)
         return self.result
 
-    def parseContent(self, link, articleID, displayMessage):
+    def __parseArticle(self, link, articleID, displayProgress):
         req = requests.get(url=str(link), cookies={"over18": "1"})
         soup = BeautifulSoup(req.text)
-        if displayMessage is True:
+        if displayProgress is True:
             print(req.url)
 
         # author
@@ -54,14 +55,12 @@ class PTTCrawler:
         except:
             ip = "ip is not find"
         # contents
-        a = str(soup.find(id="main-container").contents[1])
-        a = a.split("</div>")
+        a = str(soup.find(id="main-container").contents[1]).split("</div>")
         a = a[4].split("<span class=\"f2\">※ 發信站: 批踢踢實業坊(ptt.cc),")
         content = a[0].replace(' ', '').replace('\n', '').replace('\t', '')
         # message
-        messageNum, g, b, n, message, messageN = int(), int(), int(), int(), dict(), dict()
+        pushSummary, g, b, n, message = dict(), int(), int(), int(), list()
         for tag in soup.find_all("div", "push"):
-            messageNum += 1
             push_tag = tag.find("span", "push-tag").string.replace(' ', '')
             push_userid = tag.find("span", "push-userid").string.replace(' ', '')
             try:
@@ -71,14 +70,15 @@ class PTTCrawler:
                 push_content = ""
             push_ipdatetime = tag.find("span", "push-ipdatetime").string.replace('\n', '')
 
-            message[messageNum] = {"狀態": push_tag, "留言者": push_userid, "留言內容": push_content, "留言時間": push_ipdatetime}
-            if push_tag == '推 ':
+            message.append({"狀態": push_tag, "留言者": push_userid, "留言內容": push_content, "留言時間": push_ipdatetime})
+            if push_tag == '推':
                 g += 1
-            elif push_tag == '噓 ':
+            elif push_tag == '噓':
                 b += 1
             else:
                 n += 1
-            messageN = {"g": g, "b": b, "n": n, "all": messageNum}
+            pushSummary = {"推": g, "噓": b, "none": n, "all": len(message)}
+
         data = {"a_ID": articleID,
                 "b_作者": author,
                 "c_標題": title,
@@ -86,10 +86,13 @@ class PTTCrawler:
                 "e_ip": ip,
                 "f_內文": content,
                 "g_推文": message,
-                "h_推文總數": messageN}
+                "h_推文總數": pushSummary}
         self.result.append(data)
 
-    def getLastPageNum(self, boardName="Gossiping"):
+    def __parseContet(self, content):
+        pass
+
+    def __countLastPageNum(self, boardName="Gossiping"):
         resp = requests.get(url="http://www.ptt.cc/bbs/"+boardName+"/index.html",
                             cookies={"over18": "1"})
         if re.search("disabled\">下頁", resp.text) is not None:
