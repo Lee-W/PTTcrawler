dcc3913fa02049f66e00c502407978ccc1540f23
LeeW <cl87654321@gmail.com>
2014-08-06 22:33:01 +0800

    fix bug and add option to not display message

--- pttcrawler.py
+++ pttcrawler.py
@@ -10,13 +10,14 @@ class PTTCrawler:
         self.pttURL = "http://www.ptt.cc/"
         self.result = list()
 
-    def crawl(self, start=None, end=None, boardName="Gossiping"):
+    def crawl(self, start=None, end=None, boardName="Gossiping", displayMessage=True):
         start = self.getLastPageNum(boardName) if start is None else start
         end = self.getLastPageNum(boardName) if end is None else end
         articleID = int()
 
         for page in range(start, end+1):
-            print('index is ' + str(page))
+            if displayMessage is True:
+                print('index is ' + str(page))
             req = requests.get(url=self.pttURL+"bbs/"+boardName+"/index"+str(page)+".html",
                                cookies={"over18": "1"})
             soup = BeautifulSoup(req.text)
@@ -27,16 +28,18 @@ class PTTCrawler:
                     link = link.split("\"")
                     link = self.pttURL + link[1]
                     articleID = articleID+1
-                    self.parseContent(link, articleID)
+                    self.parseContent(link, articleID, displayMessage)
                 except:
+                    print ("error")
                     pass
             sleep(0.2)
         return self.result
 
-    def parseContent(self, link, articleID):
+    def parseContent(self, link, articleID, displayMessage):
         req = requests.get(url=str(link), cookies={"over18": "1"})
         soup = BeautifulSoup(req.text)
-        print(req.url)
+        if displayMessage is True:
+            print(req.url)
 
         # author
         author = soup.find(id="main-container").contents[1].contents[0].contents[1].string.replace(' ', '')
@@ -56,7 +59,7 @@ class PTTCrawler:
         a = a[4].split("<span class=\"f2\">※ 發信站: 批踢踢實業坊(ptt.cc),")
         content = a[0].replace(' ', '').replace('\n', '').replace('\t', '')
         # message
-        messageNum, g, b, n, message = int(), int(), int(), int(), dict()
+        messageNum, g, b, n, message, messageN = int(), int(), int(), int(), dict(), dict()
         for tag in soup.find_all("div", "push"):
             messageNum += 1
             push_tag = tag.find("span", "push-tag").string.replace(' ', '')
@@ -76,7 +79,6 @@ class PTTCrawler:
             else:
                 n += 1
             messageN = {"g": g, "b": b, "n": n, "all": messageNum}
-
         data = {"a_ID": articleID,
                 "b_作者": author,
                 "c_標題": title,
