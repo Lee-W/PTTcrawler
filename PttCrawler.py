f39d6cd17b488eabe3829006788b7ae2dfdc999c
LeeW <cl87654321@gmail.com>
2015-06-21 14:18:59 +0800

    Refactor

--- PttCrawler.py
+++ PttCrawler.py
@@ -9,48 +9,67 @@ from html2text import html2text
 
 
 class PttCrawler:
+    PTT_URL = "http://www.ptt.cc/"
+    COOKIE = {"over18": "1"}
+
     def __init__(self):
-        self.PTT_URL = "http://www.ptt.cc/"
-        self.COOKIE = {"over18": "1"}
-
         self.result = list()
-        self.boardName = str()
+        self._board_name = "Gossiping"
+        self._export_path = "output"
 
-    def crawl(self, start=None, end=None, boardName="Gossiping",
-              displayProgress=True, reserve_content_format=False):
-        self.boardName = boardName
-        start = self.get_last_page_num(boardName) if start is None else start
-        end = self.get_last_page_num(boardName) if end is None else end
+    @property
+    def board_name(self):
+        return self._board_name
+
+    @board_name.setter
+    def board_name(self, value):
+        self._board_name = value
+
+    @property
+    def export_path(self):
+        return self._export_path
+
+    @export_path.setter
+    def export_pathe(self, value):
+        self._export_path = value
+
+    def crawl(self, start=None, end=None,
+              display_progress=True, export_each=True):
+        last_page_num = self.get_last_page_num(self.board_name)
+
+        start = last_page_num if start is None else start
+        end = last_page_num if end is None else end
 
         for page in range(start, end+1):
-            if displayProgress is True:
+            if display_progress is True:
                 print('index is ' + str(page))
 
-            board_URL = self.PTT_URL+"bbs/"+boardName+"/index"+str(page)+".html"
+            board_URL = self.PTT_URL+"bbs/"+self.board_name+"/index"+str(page)+".html"
             req = requests.get(url=board_URL, cookies=self.COOKIE)
             soup = BeautifulSoup(req.text)
 
-            articleCounter = 0
+            article_counter = 0
             for tag in soup.find_all("div", "r-ent"):
                 try:
                     link = str(tag.find_all("a")).split("\"")
                     link = self.PTT_URL + link[1]
-                    articleCounter = articleCounter + 1
-                    articleID = str(page)+"-"+str(articleCounter)
-                    self.__parse_article(link, articleID, displayProgress, reserve_content_format)
+
+                    article_counter = article_counter + 1
+                    articleID = str(page)+"-"+str(article_counter)
+
+                    article = self.__parse_article(link, articleID, display_progress)
+                    if export_each:
+                        self.export_article(article)
                 except Exception as e:
                     print("Error")
                     print(e)
-                    pass
             sleep(0.2)
-        return self.result
 
-    def __parse_article(self, link, articleID, displayProgress, reserve_content_format):
+    def __parse_article(self, link, articleID, displayProgress):
         req = requests.get(url=str(link), cookies=self.COOKIE)
         soup = BeautifulSoup(req.text)
         if displayProgress is True:
             print(articleID+"  "+req.url)
-            print(link)
 
         # author
         author = soup.find(id="main-container") \
@@ -71,7 +90,7 @@ class PttCrawler:
         a = str(soup.find(id="main-container").contents[1]).split("</div>")
         a = a[4].split("<span class=\"f2\">※ 發信站: 批踢踢實業坊(ptt.cc)")
         content = a[0].replace(' ', '').replace('\n', '').replace('\t', '')
-        content = content if reserve_content_format is True else html2text(content)
+        content = html2text(content)
 
         # message
         pushSummary, g, b, n, message = dict(), int(), int(), int(), list()
@@ -106,47 +125,47 @@ class PttCrawler:
                 "g_推文": message,
                 "h_推文總數": pushSummary,
                 "i_連結": link}
-        self.result.append(data)
+        return data
 
     @staticmethod
     def __filter_space_character(content):
         return content.replace(' ', '').replace('\n', '').replace('\y', '')
 
-    def get_last_page_num(self, boardName="Gossiping"):
-        current_url = self.PTT_URL+"bbs/"+self.boardName+"/index.html"
-        resp = requests.get(url=current_url, cookies=self.COOKIE)
+    @staticmethod
+    def get_last_page_num(board_name):
+        current_url = PttCrawler.PTT_URL+"bbs/"+board_name+"/index.html"
+        resp = requests.get(url=current_url, cookies=PttCrawler.COOKIE)
         if re.search("disabled\">下頁", resp.text) is not None:
             prevPageIdentifier = re.search("index[0-9]+\.html.*上頁", resp.text).group()
             prevPage = int(re.search("[0-9]+", prevPageIdentifier).group())
         return prevPage+1
 
-    def export(self, fileName="output.json"):
-        with open(fileName, 'w') as f:
-            json.dump(self.result, f, ensure_ascii=False, indent=4, sort_keys=True)
-
-    def export_each_article(self, export_dir="output"):
+    def export_article(self, article):
         try:
-            os.makedirs(export_dir+"/"+self.boardName, exist_ok=False)
+            os.makedirs(self.export_path+"/"+self.board_name, exist_ok=True)
         except Exception:
             pass
 
-        for article in self.result:
-            path = export_dir+"/"+self.boardName+"/"+str(article["a_ID"])
-            with open(path, 'w') as f:
-                json.dump(article, f, ensure_ascii=False, indent=4, sort_keys=True)
+        file_name = self.export_path+"/"+self.board_name+"/"+str(article["a_ID"])
+        with open(file_name, "w") as f:
+            json.dump(article, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+    def export(self, fileName="output.json"):
+        with open(fileName, 'w') as f:
+            json.dump(self.result, f, ensure_ascii=False, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
     import sys
 
     ptt = PttCrawler()
+    ptt.board_name = "Gossiping"
     if len(sys.argv) == 2:
         ptt.crawl(int(sys.argv[1]))
     elif len(sys.argv) == 3:
         ptt.crawl(int(sys.argv[1]), int(sys.argv[2]))
     elif len(sys.argv) == 4:
-        ptt.crawl(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
+        ptt.board_name = sys.argv[3]
+        ptt.crawl(int(sys.argv[1]), int(sys.argv[2]))
     else:
         ptt.crawl()
-
-    ptt.export_each_article()
