6672140b43023da5f91e775768f3228934362c50
Wei Lee <weilee.rx@gmail.com>
2019-12-31 19:08:53 +0800

    style(ptt_crawler): blackify

--- ptt_crawler.py
+++ ptt_crawler.py
@@ -26,29 +26,29 @@ class PttCrawler:
         start = last_page_num if start is None else start
         end = last_page_num if end is None else end
 
-        for page in reversed(range(start, end+1)):
-            logging.info('index is ' + str(page))
+        for page in reversed(range(start, end + 1)):
+            logging.info("index is " + str(page))
 
-            page_url = f'{self.PTT_URL}bbs/{self.board_name}/index{str(page)}.html'
-            logging.info(f'Parsing {page_url}')
+            page_url = f"{self.PTT_URL}bbs/{self.board_name}/index{str(page)}.html"
+            logging.info(f"Parsing {page_url}")
             req = requests.get(url=page_url, cookies=self.COOKIE)
-            soup = BeautifulSoup(req.text, 'lxml')
+            soup = BeautifulSoup(req.text, "lxml")
 
             article_counter = 0
             for tag in soup.find_all("div", "r-ent"):
                 try:
-                    link = str(tag.find_all("a")).split("\"")
+                    link = str(tag.find_all("a")).split('"')
                     link = self.PTT_URL + link[1]
 
                     article_counter = article_counter + 1
-                    article_id = str(page)+"-"+str(article_counter)
+                    article_id = str(page) + "-" + str(article_counter)
 
                     article = self.__parse_article(link, article_id)
                     if export_each:
                         self.export_article(article)
                     self.result.append(article)
                 except AttributeError:
-                    logging.error('Article has been removed')
+                    logging.error("Article has been removed")
                 except Exception as err:
                     logging.error(err)
             sleep(0.2)
@@ -56,29 +56,33 @@ class PttCrawler:
 
     @staticmethod
     def get_last_page_num(board_name):
-        current_url = f'{PttCrawler.PTT_URL}bbs/{board_name}/index.html'
+        current_url = f"{PttCrawler.PTT_URL}bbs/{board_name}/index.html"
         resp = requests.get(url=current_url, cookies=PttCrawler.COOKIE)
         if re.search('disabled">下頁', resp.text) is not None:
             prev_page_identifer = re.search(r"index[0-9]+\.html.*上頁", resp.text).group()
             prev_page = int(re.search("[0-9]+", prev_page_identifer).group())
-        return prev_page+1
+        return prev_page + 1
 
     def __parse_article(self, link, article_id):
         req = requests.get(url=str(link), cookies=self.COOKIE)
         soup = BeautifulSoup(req.text)
-        logging.info(f'{article_id}\t{req.url}')
+        logging.info(f"{article_id}\t{req.url}")
 
         # author
         author = (
             soup.find(id="main-container")
-            .contents[1].contents[0].contents[1]
-            .text.replace(' ', '')
+            .contents[1]
+            .contents[0]
+            .contents[1]
+            .text.replace(" ", "")
         )
         # title
         title = (
             soup.find(id="main-container")
-            .contents[1].contents[2].contents[1]
-            .text.replace(' ', '')
+            .contents[1]
+            .contents[2]
+            .contents[1]
+            .text.replace(" ", "")
         )
         # date
         date = soup.find(id="main-container").contents[1].contents[3].contents[1].string
@@ -91,19 +95,19 @@ class PttCrawler:
 
         # contents
         content = str(soup.find(id="main-container").contents[1]).split("</div>")
-        content = content[4].split("<span class=\"f2\">")
-        content = content[0].replace(' ', '').replace('\n', '').replace('\t', '')
+        content = content[4].split('<span class="f2">')
+        content = content[0].replace(" ", "").replace("\n", "").replace("\t", "")
         content = PttCrawler._strip_html(content)
 
         # message
         push_summary, good, bad, none, message = dict(), int(), int(), int(), list()
         for tag in soup.find_all("div", "push"):
             try:
-                push_tag = tag.find("span", "f1 hl push-tag").string.replace(' ', '')
+                push_tag = tag.find("span", "f1 hl push-tag").string.replace(" ", "")
             except AttributeError:
-                push_tag = tag.find("span", "hl push-tag").string.replace(' ', '')
+                push_tag = tag.find("span", "hl push-tag").string.replace(" ", "")
 
-            push_userid = tag.find("span", "f3 hl push-userid").string.replace(' ', '')
+            push_userid = tag.find("span", "f3 hl push-userid").string.replace(" ", "")
 
             try:
                 push_content = tag.find("span", "f3 push-content").string
@@ -111,34 +115,42 @@ class PttCrawler:
                 # if there is no content
                 push_content = ""
 
-            push_ipdatetime = tag.find("span", "push-ipdatetime").string.replace('\n', '')
+            push_ipdatetime = tag.find("span", "push-ipdatetime").string.replace(
+                "\n", ""
+            )
 
-            message.append({"狀態": push_tag,
-                            "留言者": push_userid,
-                            "留言內容": push_content,
-                            "留言時間": push_ipdatetime})
-            if push_tag == '推':
+            message.append(
+                {
+                    "狀態": push_tag,
+                    "留言者": push_userid,
+                    "留言內容": push_content,
+                    "留言時間": push_ipdatetime,
+                }
+            )
+            if push_tag == "推":
                 good += 1
-            elif push_tag == '噓':
+            elif push_tag == "噓":
                 bad += 1
             else:
                 none += 1
             push_summary = {"推": good, "噓": bad, "none": none, "all": len(message)}
 
-        data = {"a_ID": article_id,
-                "b_作者": author,
-                "c_標題": title,
-                "d_日期": date,
-                "e_ip": ip,
-                "f_內文": content,
-                "g_推文": message,
-                "h_推文總數": push_summary,
-                "i_連結": link}
+        data = {
+            "a_ID": article_id,
+            "b_作者": author,
+            "c_標題": title,
+            "d_日期": date,
+            "e_ip": ip,
+            "f_內文": content,
+            "g_推文": message,
+            "h_推文總數": push_summary,
+            "i_連結": link,
+        }
         return data
 
     def export_article(self, article):
         try:
-            os.makedirs(self.export_path+"/"+self.board_name, exist_ok=True)
+            os.makedirs(self.export_path + "/" + self.board_name, exist_ok=True)
         except FileExistsError:
             pass
 
@@ -147,23 +159,23 @@ class PttCrawler:
             json.dump(article, f, ensure_ascii=False, indent=4, sort_keys=True)
 
     def export(self, file_name="output.json"):
-        with open(file_name, 'w') as f:
+        with open(file_name, "w") as f:
             json.dump(self.result, f, ensure_ascii=False, indent=4, sort_keys=True)
 
     @staticmethod
     def _strip_html(html):
-        content = re.compile(r'<.*?>')
-        return content.sub('', html)
+        content = re.compile(r"<.*?>")
+        return content.sub("", html)
 
 
 def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--start-page', type=int)
-    parser.add_argument('-e', '--end-page', type=int)
-    parser.add_argument('-b', '--board-name', default='Gossiping')
-    parser.add_argument('--export-each', action='store_true')
+    parser.add_argument("-s", "--start-page", type=int)
+    parser.add_argument("-e", "--end-page", type=int)
+    parser.add_argument("-b", "--board-name", default="Gossiping")
+    parser.add_argument("--export-each", action="store_true")
     args = parser.parse_args()
 
     ptt = PttCrawler()
@@ -173,5 +185,5 @@ def main():
         ptt.export()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
